ccfb8d6a80f4e140b0b56418b95095ef1068324d
Vido-Faresky <vidogamingmax@gmail.com>
2025-10-08 10:01:18 +0700

    Update semua file

---
+++ actions/destroy.php
@@ -0,0 +1,19 @@
+<?php
+    require_once '../../config/db-connection.php';
+    if(isset($_POST['destroy'])) {
+        $query="DELETE FROM users WHERE id = ?";
+
+        $userid = $_GET['id'];
+
+        $stmt = $connection->prepare($query);
+        $stmt->bind_param('i', $userid);
+        $stmt->execute();
+
+        if($stmt->affected_rows > 0) {
+            header('Location: ../../pages/users/index.php');
+            exit;
+        } else {
+            echo 'Error to delete user: ' . $stmt->error;
+        }
+    }
+?>
---
+++ actions/get-users.php
@@ -0,0 +1,17 @@
+<?php
+require_once '../../config/db-connection.php';
+
+$users = [];
+
+$query = "SELECT * from users";
+
+$stmt = $connection->prepare($query);
+$stmt->execute();
+
+$result = $stmt->get_result();
+
+while ($user = $result->fetch_assoc()) {
+    $users[] = $user;
+}
+
+?>
